46c9d17bdf6ddfe773b2602e4dbd984dd7912596
ChavesGabriella <gabriellasoeirooo@gmail.com>
2023-12-07 14:31:32 -0300

    jogo da força utilizável

--- jogo.js
+++ jogo.js
@@ -7,6 +7,7 @@ const elementos = {
     teclado: document.querySelector('.teclado'),
     palavra: document.querySelector('.palavra'),
     dica: document.querySelector('.dica'),
+
     botoes: {
         facil: document.querySelector('.botao-facil'),
         medio: document.querySelector('.botao-medio'),
@@ -14,10 +15,11 @@ const elementos = {
         cadastrar: document.querySelector('.botao-cadastrar'),
         realizarCadastro: document.querySelector('.botao-realizar-cadastro'),
         voltar: document.querySelector('.botao-voltar'),
-        reiniciar: document.querySelector('.reiniciar'),
+        reiniciar: document.querySelector('.reiniciar')
     },
+
     campos: {
-        dificuldade: {
+        dificuldade:{
             facil: document.getElementById('facil'),
             medio: document.getElementById('medio'),
             dificil: document.getElementById('dificil')
@@ -25,6 +27,7 @@ const elementos = {
         palavra: document.getElementById('palavra'),
         dica: document.getElementById('dica')
     },
+
     boneco: [
         document.querySelector('.boneco-cabeca'),
         document.querySelector('.boneco-corpo'),
@@ -32,72 +35,91 @@ const elementos = {
         document.querySelector('.boneco-braco-direito'),
         document.querySelector('.boneco-perna-esquerda'),
         document.querySelector('.boneco-perna-direita'),
-    ],
+    ]
 };
 
 const palavras = {
     facil: [{
         palavra: 'serie',
-        dica: 'pll é melhor...'
+        dica: 'Game Of Thrones é a melhor ...'
     },
     {
-        palavra: 'impar',
-        dica: 'se nao e par e...'
+        palavra: 'ímpar',
+        dica: 'Se não é par é ...'
     },
-    ],
+    {
+        palavra: 'poder',
+        dica: 'Querer não é ...'
+    },
+    {
+        palavra: 'sonho',
+        dica: 'Temos quando vamos dormir'
+    }],
     medio: [{
-        palavra: 'delineador',
-        dica: 'faz parte de produtos de maquiagem'
+        palavra: 'recesso',
+        dica: 'acontece quando o feriado é no meio da semana'
     },
     {
-        palavra: 'iluminador',
-        dica: 'faz parte de produtos de maquiagem'
+        palavra: 'traidor',
+        dica: 'judas foi'
     },
-    ],
+    {
+        palavra: 'moleque',
+        dica: 'errei, fui ...'
+    }],
     dificil: [{
-        palavra: 'flexivel',
-        dica: 'contraio de nao flexivel...'
+        palavra: 'hipócrita',
+        dica: 'reclama de algo mas faz igual'
     },
     {
-        palavra: 'exceção',
-        dica: 'é a mesma coisa que ressalva'
+        palavra: 'recíproco',
+        dica: 'todos querem um amor ...'
     },
-    ],
+    {
+        palavra: 'sufocante',
+        dica: 'lugar muito fechado é ...'
+    }]
 };
 
 function criarTeclado(){
-    const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     elementos.teclado.textContent = '';
 
     for(const letra of letras){
         const button = document.createElement('button');
+
         button.appendChild(document.createTextNode(letra.toUpperCase()));
+
         button.classList.add(`botao-${letra}`);
+
         elementos.teclado.appendChild(button);
-        button.addEventListener('click', () => {
+
+        button.addEventListener('click', () =>{
             selecionarLetra(letra);
         });
     }
-}
+};
 
 function mostrarErro(){
     const parte = elementos.boneco[5 - jogo.chances];
+
     parte.classList.remove('escondido');
 };
 
 function mostrarMensagem(vitoria){
-    const mensagem = vitoria ? '<p>Parabens!</p><p>Voce GANHOU!</p>' : '<p>Que pena!</p><p>Voce PERDEU</p>';
+    const mensagem = vitoria ? '<p>Parabens!</p><p>Você GANHOU!</p>' : '<p>Que pena!</p><p>Você PERDEU!</p>';
 
     elementos.textoMensagem.innerHTML = mensagem;
+
     elementos.telaMensagem.style.display = 'flex';
+
     elementos.telaMensagem.classList.add(`mensagem-${vitoria ? 'vitoria' : 'derrota'}`);
 
     jogo.emAndamento = false;
-};
+}
 
-
-function abrirTelacadastroPalavra(){
+function abrirTelaCadastroPalavra(){
     elementos.telaInicial.style.display = 'none';
     elementos.telaCadastro.style.display = 'flex';
 }
@@ -106,40 +128,39 @@ function voltarInicio(){
     novoJogo();
 }
 
-function cadastrarPalavra(){
-    // Obtém os valores inseridos nos campos de cadastro
+function cadastrarPalvara(){
     const novaPalavra = elementos.campos.palavra.value;
     const novaDica = elementos.campos.dica.value;
 
-     // Verifica se ambos os campos foram preenchidos
-     if (novaPalavra.trim() === '' || novaDica.trim() === '') {
-        alert('Por favor, preencha todos os campos.');
-        return;
+    var novaDificuldade = '';
+
+    if(elementos.campos.dificuldade.facil.checked){
+        novaDificuldade = 'facil';
+        //palavras.facil.palavra.push(novaPalavra);
+        //palavras.facil.dica.push(novaDica);
+        palavras.facil.push({palavra: novaPalavra, dica: novaDica});
+        console.log("Palavras; ", palavras.facil);
+    }else if(elementos.campos.dificuldade.medio.checked){
+        novaDificuldade = 'medio';
+        palavras.medio.push({palavra: novaPalavra, dica: novaDica});
+        console.log("Palavras; ", palavras.medio);
+    }else if(elementos.campos.dificuldade.dificil.checked){
+        novaDificuldade = 'dificil';
+        palavras.dificil.push({palavra: novaPalavra, dica: novaDica});
+        console.log("Palavras; ", palavras.dificil);
     }
 
-    // Adiciona a nova palavra à lista de palavras do jogo
-    palavras[jogo.dificuldade].push({ palavra: novaPalavra, dica: novaDica });
+    console.log("palavra: ", novaPalavra);
+    console.log("dica: ", novaDica);
+    console.log("dificuldade", novaDificuldade);
 
-    // Exibe uma mensagem de sucesso
-    alert('Palavra cadastrada com sucesso!');
-
-    // Volta para a tela inicial
-    voltarInicio();
+    confirm("Nova palavra cadastrada com sucesso!");
+    
 }
-
-function reiniciarJogo(){
-    elementos.botoes.reiniciar.addEventListener('click', () => novoJogo());
-    elementos.botoes.voltar.addEventListener('click', () => voltarInicio());
-    elementos.botoes.facil.addEventListener('click', () => iniciarJogo('facil'));
-    elementos.botoes.medio.addEventListener('click', () => iniciarJogo('medio'));
-    elementos.botoes.dificil.addEventListener('click', () => iniciarJogo('dificil'));
-    elementos.botoes.cadastrar.addEventListener('click', () => abrirTelacadastroPalavra());
-    elementos.botoes.realizarCadastro.addEventListener('click', () => cadastrarPalavra());
-}
-
 
 function sortearPalavra(){
     const i = Math.floor(Math.random() * palavras[jogo.dificuldade].length);
+
     const palavra = palavras[jogo.dificuldade][i].palavra;
     const dica = palavras[jogo.dificuldade][i].dica;
 
@@ -153,11 +174,13 @@ function sortearPalavra(){
 
 function mostrarPalavra(){
     elementos.dica.textContent = jogo.palavra.dica;
+
     elementos.palavra.textContent = '';
 
-    for(let i = 0; i < jogo.acertos.length; i++){
+    for(let i=0; i<jogo.acertos.length; i++){
         const letra = jogo.acertos[i].toUpperCase();
-        elementos.palavra.innerHTML += `div class="letra-${i}">${letra}</div>`;
+
+        elementos.palavra.innerHTML += `<div class = "letra-${i}">${letra}</div>`;
     }
 }
 
@@ -173,34 +196,38 @@ function novoJogo(){
         acertos: undefined,
         jogadas: [],
         chances: 6,
-        definirPalavra: function (palavra, dica){
+
+        definirPalavra: function(palavra, dica){
             this.palavra.original = palavra;
             this.palavra.tamanho = palavra.length;
             this.acertos = '';
 
-            this.palavra.semAcentos = this.palavra.original.normalize('NDF').replace(/[\u0300-\u036f]/g, '');
-            this.palavra.dica;
+            this.palavra.semAcentos = this.palavra.original.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+            this.palavra.dica = dica;
 
-            for(let i = 0; i < this.palavra.tamanho; i++){
+            for(let i=0; i<this.palavra.tamanho; i++){
                 this.acertos += ' ';
             }
         },
-        jogar: function (LetraJogada){
+
+        jogar: function(letraJogada){
             let acertou = false;
-            for(let i = 0; i < this.palavra.tamanho; i++){
+            for(let i=0; i < this.palavra.tamanho; i++){
                 const letra = this.palavra.semAcentos[i].toLowerCase();
-                if(letra === LetraJogada.toLowerCase()){
+                if(letra === letraJogada.toLowerCase()){
                     acertou = true;
 
-                    this.acertos = substituirCaractere(this.acertos, i, this.palavra.original[i]);   
+                    this.acertos = substituirCaractere(this.acertos, i, this.palavra.original[i]);
                 }
             }
 
             if(!acertou){
                 this.chances--;
             }
+
             return acertou;
         },
+
         ganhou: function(){
             return !this.acertos.includes(' ');
         },
@@ -210,7 +237,7 @@ function novoJogo(){
         acabou: function(){
             return this.ganhou() || this.perdeu();
         },
-        emAndamento: false,
+        emAndamento: false
     };
 
     elementos.telaInicial.style.display = 'flex';
@@ -226,14 +253,12 @@ function novoJogo(){
     }
 
     criarTeclado();
-   
 }
-
-novoJogo();
 
 function selecionarLetra(letra){
     if(!jogo.jogadas.includes(letra) && !jogo.acabou()){
         const acertou = jogo.jogar(letra);
+
         jogo.jogadas.push(letra);
 
         const button = document.querySelector(`.botao-${letra}`);
@@ -269,5 +294,15 @@ function substituirCaractere(str, indice, novoCaractere){
     const parteAntes = str.substring(0, indice);
     const parteDepois = str.substring(indice + 1);
     const novaString = parteAntes + novoCaractere + parteDepois;
+
     return novaString;
 }
+
+novoJogo();
+elementos.botoes.facil.addEventListener('click', () => iniciarJogo('facil'));
+elementos.botoes.medio.addEventListener('click', () => iniciarJogo('medio'));
+elementos.botoes.dificil.addEventListener('click', () => iniciarJogo('dificil'));
+elementos.botoes.reiniciar.addEventListener('click', ()=> novoJogo());
+elementos.botoes.voltar.addEventListener('click', () => voltarInicio());
+elementos.botoes.cadastrar.addEventListener('click', () => abrirTelaCadastroPalavra());
+elementos.botoes.realizarCadastro.addEventListener('click', () => cadastrarPalvara());
